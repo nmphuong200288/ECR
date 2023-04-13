52898d2b2de92064d102015d6daf64c35e02642d
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-04-13 10:55:50 +0700

    update 8

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting to deploy docker image.."
-DOCKER_IMAGE=${IMAGE_REPO_NAME}:${IMAGE_TAG}
+DOCKER_IMAGE=jenkin-pipeline-build-demo:latest
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -d -p 8080:8080 $DOCKER_IMAGE
