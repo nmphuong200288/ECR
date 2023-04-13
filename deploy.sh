848927b391d8281a5ab4d0c66ebec2ad1b6729fd
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-04-13 11:22:20 +0700

    update 9

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Starting to deploy docker image.."
+docker login "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 DOCKER_IMAGE=jenkin-pipeline-build-demo:latest
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
