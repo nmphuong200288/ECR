e8f502cc8dc1b4ed7f755f6188ed40cc9ec87e2d
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-04-13 17:43:22 +0700

    update 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('NHẬU SẾP THỌ ƠI')
+  res.send('NHẬU SẾP THỌ ƠI PRO')
 })
 
 app.get('/status', (req, res) => {
