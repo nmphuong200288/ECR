a2cb8a97284ca7f3c7ac06a087d10c45a50f2a30
Ngo Minh Phuong <minhphuong49th@gmail.com>
2023-04-13 15:16:56 +0700

    update 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('I want to drink beer on Friday of this week')
+  res.send('CICD App V2!')
 })
 
 app.get('/status', (req, res) => {
